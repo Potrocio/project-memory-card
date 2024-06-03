90fc9b2fd41998c2c63b30e5ce7f56b810daefeb
Potrocio <57481913+Potrocio@users.noreply.github.com>
2024-06-03 04:04:24 -0400

    make mobile friendly

--- src/components/game-data.jsx
+++ src/components/game-data.jsx
@@ -4,8 +4,8 @@ export default function GameData({score, highScore}) {
     return(
         <div className='game-data'>
             <div className='objective'>Select cards, just don't select the same card twice. Goodluck!</div>
-            <div className='score'>Score: {score} / 5</div>
-            <div className='high-score'>High Score: {highScore}</div>
+            <div className='score'>Score {score}</div>
+            <div className='high-score'>High Score {highScore}</div>
         </div>
     )
 }
